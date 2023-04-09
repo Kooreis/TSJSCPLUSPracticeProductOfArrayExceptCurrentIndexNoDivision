d0dfe7de9cfd668159454f750049cb1568a910a6
Kooreis <kooreis.official@gmail.com>
2023-04-09 10:32:39 -0500

    Defined a function 'productExceptSelf' that takes a vector of integers as input and returns a vector of products of all numbers in the input vector except the number at the current index.

--- solution.cpp
+++ solution.cpp
@@ -1,2 +1,13 @@
-#include <iostream>
-#include <vector>
+std::vector<int> productExceptSelf(std::vector<int>& nums) {
+    int n = nums.size();
+    std::vector<int> res(n, 1);
+    for (int i = 1; i < n; ++i) {
+        res[i] = res[i - 1] * nums[i - 1];
+    }
+    int right = 1;
+    for (int i = n - 1; i >= 0; --i) {
+        res[i] *= right;
+        right *= nums[i];
+    }
+    return res;
+}
